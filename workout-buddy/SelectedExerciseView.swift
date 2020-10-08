8617628fed2653923325768784d82b481420c911
Stepan Arsentjev <stepandel@gmail.com>
2020-10-08 10:43:12 -0400

    Timed exercises

--- workout-buddy/SelectedExerciseView.swift
+++ workout-buddy/SelectedExerciseView.swift
@@ -30,8 +30,10 @@ struct SelectedExerciseView: View {
     @State var curExIdx: Int
     
     @State private var reps: String = "0"
+    @State private var time: String = "0"
     @State private var weight: String = "0"
     @State private var sets: [SetData] = []
+    @State private var timed = false
     
     var body: some View {
         List {
@@ -40,7 +42,11 @@ struct SelectedExerciseView: View {
                 Spacer()
                 Text("Weight")
                 Spacer()
-                Text("Reps")
+                if timed {
+                    Text("Time (s)")
+                } else {
+                    Text("Reps")
+                }
             }
             .padding(.leading, 16)
             .padding(.trailing, 16)
@@ -59,16 +65,29 @@ struct SelectedExerciseView: View {
                         .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
                         .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
                     Spacer()
-                    TextField("Reps", text: self.$sets[self.sets.firstIndex(of: set)!].reps)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 80, height: 40)
-                        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                        .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
-                        .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
-                        .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    if timed {
+                        TextField("Time", text: self.$sets[self.sets.firstIndex(of: set)!].time)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            .frame(width: 80, height: 40)
+                            .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                            .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
+                            .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
+                            .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    } else {
+                        TextField("Reps", text: self.$sets[self.sets.firstIndex(of: set)!].reps)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            .frame(width: 80, height: 40)
+                            .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                            .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
+                            .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
+                            .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    }
                 }
                 .padding(.leading, 16)
                 .padding(.trailing, 16)
@@ -89,6 +108,9 @@ struct SelectedExerciseView: View {
                 result.append(nextSet)
                 return result
             })
+            if self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx][0].time != nil {
+                self.timed = true
+            }
         }
         .onDisappear {
             self.saveSets()
