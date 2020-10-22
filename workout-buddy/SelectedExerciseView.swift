25b81755114b95fdcb8ce274bfcb1035ba0bc4ba
Stepan Arsentjev <stepandel@gmail.com>
2020-10-22 14:02:56 -0400

    Fixed: crashing when deleting sets

--- workout-buddy/SelectedExerciseView.swift
+++ workout-buddy/SelectedExerciseView.swift
@@ -14,6 +14,7 @@ struct SetData: Hashable, Identifiable {
     var reps: String
     var time: String
     var weight: String
+    var deleted: Bool
     
     init(exId: String, reps: String, time: String, weight: String) {
         self.id = UUID()
@@ -21,6 +22,7 @@ struct SetData: Hashable, Identifiable {
         self.reps = reps
         self.time = time
         self.weight = weight
+        self.deleted = false
     }
 }
 
@@ -51,22 +53,11 @@ struct SelectedExerciseView: View {
             .padding(.leading, 16)
             .padding(.trailing, 16)
             ForEach(self.sets) { set in
-                HStack {
-                    Text("\(self.sets.firstIndex(of: set)! + 1)")
-                    Spacer()
-                    TextField("0 kg", text: self.$sets[self.sets.firstIndex(of: set)!].weight)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                        .frame(width: 80, height: 40)
-                        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                        .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
-                        .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
-                        .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
-                    Spacer()
-                    if timed {
-                        TextField("0 sec", text: self.$sets[self.sets.firstIndex(of: set)!].time)
+                if !set.deleted {
+                    HStack {
+                        Text("\(self.sets.firstIndex(of: set)! + 1)")
+                        Spacer()
+                        TextField("0 kg", text: self.$sets[self.sets.firstIndex(of: set)!].weight)
                             .multilineTextAlignment(.center)
                             .font(.system(size: 20, weight: .semibold))
                             .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
@@ -76,24 +67,45 @@ struct SelectedExerciseView: View {
                             .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
                             .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
                             .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
-                    } else {
-                        TextField("0", text: self.$sets[self.sets.firstIndex(of: set)!].reps)
-                            .multilineTextAlignment(.center)
-                            .font(.system(size: 20, weight: .semibold))
-                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .frame(width: 80, height: 40)
-                            .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-                            .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
-                            .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
-                            .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                        Spacer()
+                        if timed {
+                            TextField("0 sec", text: self.$sets[self.sets.firstIndex(of: set)!].time)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 20, weight: .semibold))
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                .frame(width: 80, height: 40)
+                                .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
+                                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
+                                .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                        } else {
+                            TextField("0", text: self.$sets[self.sets.firstIndex(of: set)!].reps)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 20, weight: .semibold))
+                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                .frame(width: 80, height: 40)
+                                .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 0.5, x: 0.5, y: 0.5)
+                                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 0.5, x: -0.5, y: -0.5)
+                                .padding(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                        }
                     }
+                    .padding(.leading, 16)
+                    .padding(.trailing, 16)
                 }
-                .padding(.leading, 16)
-                .padding(.trailing, 16)
             }
             .onDelete { (offset) in
-                self.sets.remove(atOffsets: offset)
+                let nonDeletedSets = self.sets.filter { !$0.deleted }
+                if nonDeletedSets.count > 1 {
+                    offset.forEach { i in
+                        var deletedSet = self.sets[i]
+                        deletedSet.deleted = true
+                        self.sets.remove(at: i)
+                        self.sets.append(deletedSet)
+                    }
+                }
             }
             Button(action: {
                 self.addSet()
@@ -120,16 +132,14 @@ struct SelectedExerciseView: View {
     func addSet() {
         let firstSet = self.sets[0]
         let newSet = SetData(exId: firstSet.exId, reps: firstSet.reps, time: firstSet.time, weight: firstSet.weight)
-        self.sets.append(newSet)
+        let nonDeletedSets = self.sets.filter{ !$0.deleted }
+        self.sets.insert(newSet, at: nonDeletedSets.count)
     }
     
     func saveSets() {
+        self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx] = []
         (0..<self.sets.count).forEach { i in
-            if (self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx].indices.contains(i)) {
-                self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx][i].reps = Int(self.sets[i].reps)
-                self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx][i].time = Int(self.sets[i].time)
-                self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx][i].weight = Int(self.sets[i].weight)
-            } else {
+            if !self.sets[i].deleted {
                 let newExSet = ExSet(exId: self.sets[i].exId, time: Int(self.sets[i].time), reps: Int(self.sets[i].reps), weight: Int(self.sets[i].weight))
                 self.trackWorkoutViewModel.workout.rounds[self.currentRound].sets[self.curExIdx].append(newExSet)
             }
